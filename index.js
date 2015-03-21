d7d7a7da947205df900ab8598aa83e1747c52db1
Nicklas Nygren <hello@nickl.as>
2015-03-21 13:31:57 +0100

    Create deprecation issues for selectors

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ var args          = process.argv.slice(2);
 var cmd           = args.shift();
 var configPath    = process.env.HOME + '/.brisk-content.config.json';
 var contentServer = require('./lib/contentServer.js');
-var config, server;
+var GithubAPI     = require('github');
+var config, configPromptSchema, server, github;
 
-var configPromptSchema = [{
+github = new GithubAPI({
+  version: '3.0.0'
+});
+
+configPromptSchema = [{
   name: 'contentServerAddress',
   type: 'string',
   description: 'Content server address'
@@ -16,6 +21,10 @@ var configPromptSchema = [{
   name: 'cardRepo',
   type: 'string',
   description: 'Card repo'
+}, {
+  name: 'githubAccessToken',
+  type: 'string',
+  description: 'Github access token'
 }];
 
 try {
@@ -25,7 +34,6 @@ try {
   main();
 }
 catch (err) {
-  config = {};
   console.log('No config file found in home directory. Let\'s create one.');
   prompt.start();
 
@@ -35,9 +43,8 @@ catch (err) {
       if (err) {
         //
       }
-      config.cardRepo             = result.cardRepo;
-      config.contentServerAddress = result.contentServerAddress;
-
+      config = result;
+      console.log(result);
       saveConfig();
       main();
     }
@@ -47,9 +54,17 @@ catch (err) {
 function main () {
   server = contentServer(config.contentServerAddress);
 
+  github.authenticate({
+    type: 'oauth',
+    token: config.githubAccessToken
+  });
+
   switch (cmd) {
     case 'querySelector':
       performQuerySelectorRequest(args[0]);
+      break;
+    case 'deprecateSelector':
+      deprecateSelector(args[0], args[1]);
       break;
     case 'help':
       default:
@@ -74,6 +89,49 @@ function performQuerySelectorRequest (selector) {
         lastOrgId = card.orgId;
       }
       console.log('\t- ' + card.cardname);
+    });
+  });
+};
+
+function deprecateSelector (selector, milestone) {
+  server.querySelector(selector, function (cards) {
+    var lastOrgId, issuesCreated = 0;
+
+    if (!cards.length) {
+      console.log('Found no matches');
+      return;
+    }
+
+    console.log('Found ' + cards.length + ' matches for "' + selector + '":\n');
+
+    cards.forEach(function (card) {
+      if (lastOrgId !== card.orgId) {
+        console.log('OrgId ' + card.orgId + ':');
+        lastOrgId = card.orgId;
+      }
+      console.log('\t- ' + card.cardname);
+    });
+    console.log('\nCreating github issues...');
+
+    cards.forEach(function (card, idx) {
+      github.issues.create({
+        user: 'dexplora',
+        repo: config.cardRepo,
+        title: '[OrgId '+ card.orgId + ']: Resolve deprecated directive "' + selector + '" on '+ card.cardname,
+        milestone: milestone,
+        labels: ['deprecated', 'template']
+      }, function (err, data) {
+        if (err) {
+          console.log(JSON.parse(err).message);
+        }
+        else if (data) {
+          issuesCreated++;
+        }
+
+        if (idx === cards.length-1) {
+          console.log('Done!');
+        }
+      });
     });
   });
 };
